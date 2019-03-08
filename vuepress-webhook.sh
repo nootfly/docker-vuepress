88d9b8ec600da5716c32340a55a672d0e7796832
Noot <nootfly@users.noreply.github.com>
2019-03-08 13:00:17 +1100

    Update vuepress-webhook.sh

--- vuepress-webhook.sh
+++ vuepress-webhook.sh
@@ -3,5 +3,5 @@ mkdir /root/src
 pushd /root/src
 git clone $VUEPRESS_REPO .; git pull
 vuepress build
-pushd .vuepress; mv /root/html /root/html.old; mv dist /root/html; rm -rf /root/html.old; popd
+pushd .vuepress; mv /root/html/dist /root/html/dist.old; mv dist /root/html/dist; rm -rf /root/html/dist.old; popd
 popd
